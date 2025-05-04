606ea0b38b8f374fa57f90b645fe947e0c27f3d0
Revanth Pothukuchi <revanthpothukuchi123@gmail.com>
2025-05-04 17:28:24 -0400

    feat: updated find-intervals to include video ids

--- find-intervals.py
+++ find-intervals.py
@@ -1,22 +1,25 @@
 import pandas as pd
-import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 from collections import defaultdict
 import ast
 
 
-def timestamp_to_seconds(t):
+def timestamp_to_seconds(t: str):
     """Convert HH:MM:SS.FF to seconds"""
     t = datetime.strptime(t, "%H:%M:%S.%f")
     return t.hour * 3600 + t.minute * 60 + t.second + t.microsecond / 1e6
+
+
+def seconds_to_timestamp(seconds: float):
+    """Convert HH:MM:SS.FF to seconds"""
+    return str(timedelta(seconds=seconds))
+
 
 def find_free_1s_intervals(df, step=1.0):
     df = df.copy()
     df["start_sec"] = df["start_timestamp"].apply(timestamp_to_seconds)
     df["stop_sec"] = df["stop_timestamp"].apply(timestamp_to_seconds)
-
     result = []
-
     for video_id, group in df.groupby("video_id"):
         min_time = group["start_sec"].min()
         max_time = group["stop_sec"].max()
@@ -35,20 +38,22 @@ def find_free_1s_intervals(df, step=1.0):
 
             if len(overlaps) == 1:
                 all_nouns = overlaps.iloc[0]["all_nouns"]
-                lst = ast.literal_eval(all_nouns)
-                if len(lst) == 1:
+                narration_id = overlaps.iloc[0]["narration_id"]
+                participant_id = overlaps.iloc[0]["participant_id"]
+                nouns = ast.literal_eval(all_nouns)
+                if len(nouns) == 1:
                     result.append(
                         {
+                            "narration_id": narration_id,
+                            "participant_id": participant_id,
                             "video_id": video_id,
-                            "window_start": round(window_start, 2),
-                            "window_end": round(window_end, 2),
-                            "middle_start": round(middle_start, 2),
-                            "middle_end": round(middle_end, 2),
+                            "start_timestamp": seconds_to_timestamp(round(window_start, 2)),
+                            "end_timestamp": seconds_to_timestamp(round(window_end, 2)),
                             "all_nouns": all_nouns,
                         }
                     )
 
-            t += step  # slide window forward
+            t += step
 
     return pd.DataFrame(result)
 
@@ -56,7 +61,7 @@ def find_free_1s_intervals(df, step=1.0):
 if __name__ == "__main__":
     # annotations = pd.read_csv("metadata/annotations.csv")
     # intervals = find_free_1s_intervals(annotations)
-    # intervals.to_csv('metadata/intervals.csv')
+    # intervals.to_csv('metadata/intervals.csv', index=False)
     
     intervals = pd.read_csv("metadata/intervals.csv")
     all_nouns = intervals['all_nouns']
